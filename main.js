1bd25c2f628cac860aacaacdb5f2e3093c0ea96d
Patsy Lascano <patsy@Patsys-MacBook-Pro.local>
2017-11-27 22:36:20 +0800

    cleaned up a bunch of stuff

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var rectGrid = (function() {
   var rectMasks = ["link-2", "three-corner-2"];
   var colors = ["#7537f6","#acffca","#999999","#fff"];
   var gridSize = 50;
-  var shapeNo = 50;
+  var shapeNo = 10;
   var width = 1000;
   var height = 650;
   var shapeArray = [];
@@ -18,22 +18,58 @@ var rectGrid = (function() {
         viewBox: [0, 0, width, height]
     });
 
+    drawCanvas();
     populateCanvas();
-    setInterval(populateCanvas, 1000);
-    setInterval(animation, 500);
+    //setInterval(populateCanvas, 5000);
+    setInterval(animateRect, 2000);
 
   };
 
-  function animation() {
+  function animateRect() {
+
+    _s.clear();
+    //shapeArray = [];
+
+    drawCanvas();
+
+    for (var i = 0; i < shapeArray.length-1; i++) {
+      if (!checkOverlap(shapeArray[i], shapeArray[i+1]) || !checkOutOfBounds(shapeArray[i])) {
+        shapeArray[i].update();
+      }
+    }
+
     shapeArray.forEach(function(child) {
-      child.update();
+      child.show();
     });
+
   }
 
   function populateCanvas() {
 
-    _s.clear();
-    shapeArray = [];
+    while (shapeArray.length < shapeNo) {
+
+      var overlapping = false;
+      var newShape = new Shape();
+
+      for (var i = 0; i < shapeArray.length; i++) {
+        if (checkOverlap(newShape, shapeArray[i])) {
+          overlapping = true;
+        }
+      }
+
+      if (!overlapping && !checkOutOfBounds(newShape)) {
+        shapeArray.push(newShape);
+      }
+
+    }
+
+    shapeArray.forEach(function(child) {
+      child.show();
+    });
+
+  }
+
+  function drawCanvas() {
 
     //grid pattern from SnapSVGgrid by ajpaul, https://github.com/ajpaul/SnapSVGgrid
 
@@ -46,110 +82,96 @@ var rectGrid = (function() {
     //apply pattern
     roomOutline.attr({ fill: pattern });
 
-    while (shapeArray.length < shapeNo) {
-
-      var overlapping = false;
-      var newShape = new Shape();
-
-      for (var i = 0; i <= shapeArray.length-1; i++) {
-        if (checkOverlap(newShape, shapeArray[i])) {
-          overlapping = true;
-        }
-      }
-
-      if (!overlapping) {
-        shapeArray.push(newShape);
-      }
-    }
-    shapeArray.forEach(function(child) {
-      child.show();
-    });
   }
 
   function Shape() {
     this.rect;
-    this.x = (Math.floor(Math.random() * (width/gridSize)) + 1) * gridSize;
-    this.y = (Math.floor(Math.random() * (height/gridSize)) + 1) * gridSize;
+    this.mask;
+    this.color = colors[randomNumber(colors)];
+    this.x = (Math.floor(Math.random() * (width/gridSize))) * gridSize;
+    this.y = (Math.floor(Math.random() * (height/gridSize))) * gridSize;
     this.maxX;
     this.maxY;
-    this.bigSquare = false;
-    this.longRect = false;
-    this.isSquare = false;
 
-    var random = Math.floor(Math.random() * 3 ) + 1;
+    var smallSquare = false;
+    var random = Math.floor(Math.random() * 2 ) + 1;
 
     //rect or square
     if (Math.random() > 0.8) {
       this.maxX = this.x + (gridSize * 3 * random);
       this.maxY = this.y + (gridSize * 2 * random);
+      this.mask = assignMask(rectMasks,false);
     }
     else if (Math.random() > 0.8) {
       this.maxX = this.x + (gridSize * random);
       this.maxY = this.y + (gridSize * random);
-      this.bigSquare = true;
+      this.mask = assignMask(squareMasks,true);
     }
     else if (Math.random() > 0.8) {
       this.maxX = this.x + (gridSize * 2 * random);
       this.maxY = this.y + (gridSize * 4 * random);
-      this.longRect = true;
+      this.mask = "link-3"
     }
     else {
       this.maxX = this.x + gridSize;
       this.maxY = this.y + gridSize;
-      this.isSquare = true;
-    }
-
-    //check if out of bounds
-
-    if (this.maxX >= width) {
-      this.x = this.x - (this.maxX - this.x);
-      this.maxX = this.maxX - (this.maxX - this.x);
-    }
-
-    if (this.maxY >= height) {
-      this.y = this.y - (this.maxY - this.y);
-      this.maxY = this.maxY - (this.maxY - this.y);
+      this.mask = assignMask(squareMasks,true);
+      smallSquare = true;
     }
 
     //check if small square and make smaller squares randomly
 
-    if ((this.maxX - this.x) == (this.maxY - this.y) ) {
-      this.isSquare = true;
-      if (!this.bigSquare) {
-        if (Math.random() > 0.2) {
-          this.x = (Math.floor(Math.random() * (width/(gridSize/2))) + 1) * (gridSize/2);
-          this.y = (Math.floor(Math.random() * (height/(gridSize/2))) + 1) * (gridSize/2);
-          this.maxX = this.x + (gridSize/2);
-          this.maxY = this.y + (gridSize/2);
-        }
-      }
+    if (smallSquare && Math.random() > 0.2) {
+      this.x = (Math.floor(Math.random() * (width/(gridSize/2)))) * (gridSize/2);
+      this.y = (Math.floor(Math.random() * (height/(gridSize/2)))) * (gridSize/2);
+      this.maxX = this.x + (gridSize/2);
+      this.maxY = this.y + (gridSize/2);
     };
 
     this.update = function() {
-      if (Math.random() > 0.3) {
-        if (this.isSquare) {this.rect.transform("r" + (Math.floor(Math.random()* 4) * 90).toString())}
-        else {this.rect.transform('s1,' + (Math.random() < 0.5 ? -1 : 1).toString())};
-      }
-      if (Math.random() > 0.3) {
-        this.rect.attr({ fill: colors[randomNumber(colors)] });
-      }
+      // var randomZero = Math.random() < 0.5 ? gridSize : 0;
+      // if (this.maxY > height || this.maxX > width) {
+      //   this.maxY -= randomZero;
+      //   this.y -= randomZero;
+      //
+      //   this.maxX -= randomZero;
+      //   this.x -= randomZero;
+      // }
+      // else if (this.y < 0 || this.x < 0) {
+      //   this.maxY += randomZero;
+      //   this.y += randomZero;
+      //
+      //   this.maxX += randomZero;
+      //   this.x += randomZero;
+      // }
+      // else {
+        var randomSubAdd = Math.random() < 0.5 ? -gridSize : gridSize;
+        this.x += randomSubAdd;
+        this.maxX += randomSubAdd;
+
+        this.maxY += randomSubAdd;
+        this.y += randomSubAdd;
+      //}
     }
 
-    this.show = function() {
-      this.rect = _s.rect(this.x,this.y,this.maxX - this.x,this.maxY - this.y).attr({
-        mask: (this.isSquare) ?
-                _s.image("svg/" + assignMask(squareMasks,true) + ".svg",this.x,this.y,this.maxX - this.x,this.maxY - this.y) :
-                (this.longRect) ?
-                _s.image("svg/link-3.svg",this.x,this.y,this.maxX - this.x,this.maxY - this.y) :
-                _s.image("svg/" + assignMask(rectMasks,false) +".svg",this.x,this.y,this.maxX - this.x,this.maxY - this.y),
-                fill: colors[randomNumber(colors)] });
-    };
+    // if (Math.random() > 0.3) {
+    //   if (this.isSquare) {this.rect.transform("r" + (Math.floor(Math.random()* 4) * 90).toString())}
+    //   else {this.rect.transform('s1,' + (Math.random() < 0.5 ? -1 : 1).toString())};
+    // }
+    // if (Math.random() > 0.3) {
+    //   this.rect.attr({ fill: colors[randomNumber(colors)] });
+    // }
 
+    this.show = function() {
+
+      this.rect = _s.rect(this.x,this.y,this.maxX-this.x,this.maxY-this.y).attr({
+        mask: _s.image("svg/" + this.mask + ".svg",this.x,this.y,this.maxX - this.x,this.maxY - this.y),
+        fill: this.color });
+    };
   }
 
   function assignMask(maskArray,isSquare) {
-    var mask;
-    mask = maskArray[randomNumber(maskArray)];
+    var mask = maskArray[randomNumber(maskArray)];
     if (isSquare) {
       var masks = maskArray.concat(specialMasks);
       mask = maskArray[randomNumber(maskArray)];
@@ -162,6 +184,14 @@ var rectGrid = (function() {
         testShape.x < prevShape.maxX &&
         testShape.maxY > prevShape.y &&
         testShape.y < testShape.maxY) {
+      return true;
+    }
+    return false;
+  }
+
+  function checkOutOfBounds(testShape) {
+    if (testShape.maxX > width || testShape.maxY > height ||
+        testShape.x < 0 || testShape.y < 0) {
       return true;
     }
     return false;
